b4f96d7d7d420aac8a6165a6f73be1570dbb349a
rongtao <17710497202@163.com>
2017-11-23 18:02:36 +0800

    init commit
    init commit

---
+++ app/src/javascripts/main.js
@@ -0,0 +1,90 @@
+(function () {
+    'use strict';
+
+    // load dependencies
+    var animationControl = require('./animation-control.js');
+
+    $(document).ready(function () {
+        // 下方导航 menu-main 的切换
+        $(".munu-li").each(function (index) {
+            $(this).click(function () {
+                // 导航 action
+                $("li").removeClass ("action");
+                $(this).addClass ("action");
+                // 页面 action
+                $(".page").removeClass("action");
+                $(".page").eq(index).addClass("action");
+                // 页面切换
+                $(".page").css("display", "none");
+                $(".page.action").css("display", "block");
+            });
+        });
+        // 户型鉴赏查看详情按钮打开户型鉴赏详情页
+        $(".hx-btn").click(function(){
+            $(".page-hx-xiangqing").css("display", "block");
+        })
+        $(".hx-xq-back").click(function(){
+            $(".page-hx-xiangqing").css("display", "none");
+        })
+
+        var bgMusic = $('audio').get(0);
+        var $btnMusic = $('.btn-music');
+        var $upArrow = $('.up-arrow');
+
+        // background music control
+        $btnMusic.click(function () {
+            if (bgMusic.paused) {
+                bgMusic.play();
+                $(this).removeClass('paused');
+            } else {
+                bgMusic.pause();
+                $(this).addClass('paused');
+            }
+        });
+
+        // init Swiper
+        new Swiper('.swiper-container', {
+            mousewheelControl: true,
+            effect: 'coverflow',    // slide, fade, coverflow or flip
+            speed: 400,
+            direction: 'vertical',
+            fade: {
+                crossFade: false
+            },
+            coverflow: {
+                rotate: 100,
+                stretch: 0,
+                depth: 300,
+                modifier: 1,
+                slideShadows: false     // do disable shadows for better performance
+            },
+            flip: {
+                limitRotation: true,
+                slideShadows: false     // do disable shadows for better performance
+            },
+            onInit: function (swiper) {
+                animationControl.initAnimationItems();  // get items ready for animations
+                animationControl.playAnimation(swiper); // play animations of the first slide
+            },
+            onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
+                if (swiper.activeIndex === swiper.slides.length - 1) {
+                    $upArrow.hide();
+                } else {
+                    $upArrow.show();
+                }
+            },
+            onTransitionEnd: function (swiper) {       // play animations of the current slide
+                animationControl.playAnimation(swiper);
+            },
+            onTouchStart: function (swiper, event) {    // mobile devices don't allow audios to play automatically, it has to be triggered by a user event(click / touch).
+                if (!$btnMusic.hasClass('paused') && bgMusic.paused) {
+                    bgMusic.play();
+                }
+            }
+        });
+
+        // hide loading animation since everything is ready
+        // 通过使用滑动效果，隐藏被选元素，如果元素已显示出来的话。
+        $('.loading-overlay').slideUp();
+    });
+})();
